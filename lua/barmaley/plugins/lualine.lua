c76ea5104e553647e0f475984db28d91fa196d63
Denis Rogozin <133758633+Barmaley0@users.noreply.github.com>
2025-01-29 16:50:09 +0300

    29.01.2025

--- lua/barmaley/plugins/lualine.lua
+++ lua/barmaley/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        theme = "catppuccin",
+        theme = "everforest",
         globalstatus = true,
         -- icons_enabled = true,
         component_separators = { left = "", right = "" },
